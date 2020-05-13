ec9c3537756813481f52c102b5fe72e580dd697f
Alexander Chori <alexandrchori@gmail.com>
2020-05-13 15:08:53 +0300

    Don't destroy client session

--- examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+++ examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -91,7 +91,9 @@ public class HelloWorldClient {
         .build();
     try {
       HelloWorldClient client = new HelloWorldClient(channel);
-      client.greet(user);
+      while (true) {
+        client.greet(user);
+      }
     } finally {
       // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
       // resources the channel should be shut down when it will no longer be used. If it may be used
